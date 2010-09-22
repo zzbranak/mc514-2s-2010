019504e9b6994f4d447a11d571288cfb959b570e
Albert.Wright4 <Albert.Wright4@69602c06-05b7-cd18-fa00-c4cc009c0520>
2010-09-22 19:41:42 +0000

    Retoques dados. identação, comentários e afins.

--- threads/ThreadCB.java
+++ threads/ThreadCB.java
@@ -65,22 +65,24 @@ public class ThreadCB extends IflThreadCB
     */
     static public ThreadCB do_create(TaskCB task)
     {
+    	/* Testa se a task tem menos que o máximo de threads permitidas */
 	    if(task.getThreadCount() < IflThreadCB.MaxThreadsPerTask) {
 	    	
             ThreadCB thread = new ThreadCB();
 
-			if(task.addThread(thread) == FAILURE) {
+	        if(task.addThread(thread) == FAILURE) {
 			    return null;
 			}
-					
+		    /* Atribui a thread à task, seta seu estado, coloca na lista */
+	        /* de ready, e despacha uma nova thread                      */
 			thread.setTask(task);	
 			thread.setStatus(ThreadReady);
 			ThreadCB.ReadyQueue.append(thread);
-			ThreadCB.dispatch();
+			dispatch();
 			return thread;
 
 			} else {
-				  ThreadCB.dispatch();
+				  dispatch();
 			      return null;
 			}
     }
@@ -105,15 +107,22 @@ public class ThreadCB extends IflThreadCB
     	int i;
     	Device cDev;
     	
+    	//Se o estado da thread a matar é ready, tira ela da ReadyQueue
+    	//e muda seu estado para ThreadKill
     	if(this.getStatus() == ThreadReady){
 			ThreadCB.ReadyQueue.remove(this);
 			this.setStatus(ThreadKill);
     	}
+    	//Se o estado da thread a matar é running, tira ela da CPU e seta
+    	//seu estado para ThreadKill
     	if(this.getStatus() == ThreadRunning){
         	this.getTask().setCurrentThread(null);
         	MMU.setPTBR(null);
 			this.setStatus(ThreadKill);
     	}
+    	/* Se o estado da thread a matar é waiting, varre a lista de Devices */
+    	/* cancelando qualquer IO pendente que a thread requisitou em cada   */
+    	/* Device e depois seta o estado da thread para ThreadKill           */
     	if(this.getStatus() >= ThreadWaiting){
     		for(i=0;i<nDevices;i++){
     			cDev = Device.get(i);
@@ -122,6 +131,9 @@ public class ThreadCB extends IflThreadCB
 			this.setStatus(ThreadKill);
     	}
     	
+    	/* A thread morta abre mao de seus recursos alocados, despachamos */
+    	/* uma nova, removemos a thread morta da task e matamos a task se */
+    	/* Necessário */
     	ResourceCB.giveupResources(this);    	
         dispatch();
         OwnerTask.removeThread(this);
@@ -150,9 +162,7 @@ public class ThreadCB extends IflThreadCB
     public void do_suspend(Event event)
     {
      
-        /*int status = this.getStatus();*/
-        
-    		/* Verifica status da thread */
+   		/* Verifica status da thread */
         if(this.getStatus() == ThreadRunning) {
         	this.setStatus(ThreadWaiting);
         	this.getTask().setCurrentThread(null);
@@ -179,24 +189,21 @@ public class ThreadCB extends IflThreadCB
     */
     public void do_resume()
     {    
-    	
-    	
-			if(getStatus() >= ThreadWaiting) {
-				/*this.setStatus(ThreadWaiting-1);*/
-				if(this.getStatus() == ThreadWaiting) {
-					this.setStatus(ThreadReady);
-					ThreadCB.ReadyQueue.append(this);
-    		    }else {
-					this.setStatus(this.getStatus()-1);
-				}
-					dispatch();
-			}else{
-				MyOut.print("osp.Threads.ThreadsCB", "Tentativa de resumir uma Thread" +
-						    " que não estava esperando"); 
-				return;
-			}
+    	 /* Se o estado da thread for igual a Waiting, muda para Ready e  */ 	
+    	 /* coloca ela na ReadyQueue. Se for maior, desce em 1 o nível de */
+    	 /* Waiting                                                       */
+	    if(getStatus() >= ThreadWaiting) {
+            if(this.getStatus() == ThreadWaiting) {
+		        this.setStatus(ThreadReady);
+			    ThreadCB.ReadyQueue.append(this);
+			
+		    }else this.setStatus(this.getStatus()-1);
+        
+		    dispatch();
+		
+	    }else return;
 
-		}
+	}
 
     /** 
         Selects a thread from the run queue and dispatches it. 
@@ -213,46 +220,43 @@ public class ThreadCB extends IflThreadCB
     */
     public static int do_dispatch()
     {
-    	/*int stat;
     	
-    	stat = this.getStatus();
-        MyOut.print("osp.Threads.ThreadsCB", ">>>>"+ThreadRunning);    	
-        MyOut.print("osp.Threads.ThreadsCB", "===="+stat);*/
+        TaskCB    currentTask;
+    	ThreadCB  currentThread;
+    	PageTable currentPage;
+    	TaskCB 		newTask;
+    	ThreadCB 	newThread;
+    	PageTable newPage;
 
-				TaskCB    currentTask;
-				ThreadCB  currentThread;
-				PageTable currentPage;
-				TaskCB 		newTask;
-				ThreadCB 	newThread;
-				PageTable newPage;
-				/* Verifica se existe uma thread na ReadyQueue 
-				 * e faz a troca de contexto */
+        /* pega o PTBR da task atual no processador */
+    	currentPage = MMU.getPTBR();
+        
+    	/* Se ouver uma thread rodando, tira ela da CPUe coloca ela na */
+    	/* ReadyQueue se necessário */
+    	if(currentPage != null) {
+    		currentTask = currentPage.getTask();
+    		currentThread = currentTask.getCurrentThread();
+    		if(currentThread.getStatus() == ThreadRunning){
+    			currentThread.setStatus(ThreadReady);
+    			ThreadCB.ReadyQueue.append(currentThread);
+    		}
+    		MMU.setPTBR(null);
+    		currentTask.setCurrentThread(null);
+    	}
+        
+    	/* Tira uma thread da ReadyQueue e coloca ela para rodar segundo    */
+    	/* o algoritmo first come first serve. Com o timer, torna-se método */
+    	/* de scheduling round-robin*/
+    	newThread = (ThreadCB)ThreadCB.ReadyQueue.removeHead();
+    	if(newThread == null) return FAILURE;
 
-				/* Seta o status ThreadReady na thread atual */
-				currentPage = MMU.getPTBR();
-				
-				if(currentPage != null) {
-					currentTask = currentPage.getTask();
-					currentThread = currentTask.getCurrentThread();
-					if(currentThread.getStatus() == ThreadRunning){
-					    currentThread.setStatus(ThreadReady);
-					    ThreadCB.ReadyQueue.append(currentThread);
-					}
-			    MMU.setPTBR(null);
-				currentTask.setCurrentThread(null);
-				}
 
-				newThread = (ThreadCB)ThreadCB.ReadyQueue.removeHead();
-				if(newThread == null) return FAILURE;
-
-				/* Coloca a nova thread para rodar, utilizando
-			 	* algoritmo First In, First Served  */
-				newThread.setStatus(ThreadRunning);
-				newTask = newThread.getTask();
-				newPage = newTask.getPageTable();
-				MMU.setPTBR(newPage);
-				newTask.setCurrentThread(newThread);
-				return SUCCESS;
+    	newThread.setStatus(ThreadRunning);
+    	newTask = newThread.getTask();
+    	newPage = newTask.getPageTable();
+    	MMU.setPTBR(newPage);
+    	newTask.setCurrentThread(newThread);
+    	return SUCCESS;
 
     }
 
